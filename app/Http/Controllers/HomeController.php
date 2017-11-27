2d8bfa01851b5e9902b3ea964bd35956eea0859d
mcrodro <rodrigovallaro@hotmail.com>
2017-11-26 21:47:02 -0300

    -eliminado el echo de debug en la HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,6 @@ class HomeController extends Controller
     public function index()
     {    
         if ( Cache::has('id_cancha') ){
-            echo "existen datos en la cache";
             $id_cancha = Cache::get('id_cancha'); 
             $id_turno = Cache::get('id_turno');
             $fecha = Cache::get('fecha');
@@ -34,7 +33,6 @@ class HomeController extends Controller
         }
         else
         {
-            echo "NO existen datos en la cache";
             return view('index');//home
         }
         
